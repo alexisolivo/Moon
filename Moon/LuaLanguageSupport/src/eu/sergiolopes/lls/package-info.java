066659132a72e98f71c13984441259947bdde08b
Sérgio Lopes <knitter.is@gmail.com>
2013-07-13 12:35:06 +0100

    Updating license headers.

--- Moon/LuaLanguageSupport/src/eu/sergiolopes/lls/package-info.java
+++ Moon/LuaLanguageSupport/src/eu/sergiolopes/lls/package-info.java
@@ -1,3 +1,24 @@
+/* 
+ * This file is part of Moon, a Lua/Corona SDK group of modules for the 
+ * NetBeans IDE.
+ * 
+ * Copyright (c) 2013, Sérgio Lopes
+ * 
+ * Moon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Moon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * https://github.com/Knitter/Moon
+ */
 @TemplateRegistration(folder = "Other", content = "resources/LuaSourceTemplate.lua")
 package eu.sergiolopes.lls;
 
--- Moon/LuaLanguageSupport/src/eu/sergiolopes/lls/parser/LParser.java
+++ Moon/LuaLanguageSupport/src/eu/sergiolopes/lls/parser/LParser.java
@@ -1,11 +1,29 @@
+/* 
+ * This file is part of Moon, a Lua/Corona SDK group of modules for the 
+ * NetBeans IDE.
+ * 
+ * Copyright (c) 2013, Sérgio Lopes
+ * 
+ * Moon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Moon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * https://github.com/Knitter/Moon
+ */
 package eu.sergiolopes.lls.parser;
 
 import eu.sergiolopes.lls.parser.jcc.LuaParser;
-import eu.sergiolopes.lls.parser.jcc.ParseException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.event.ChangeListener;
 import org.netbeans.modules.parsing.api.Snapshot;
 import org.netbeans.modules.parsing.api.Task;
